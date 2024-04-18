96b016258dd26c681d21eaff73294b45c09b0698
Lucator53 <160589680+Lucator53@users.noreply.github.com>
2024-04-18 10:49:11 +0800

    Add files via upload

---
+++ assets/javascript/sidebar.js
@@ -0,0 +1,43 @@
+var sidebarOpen = false;
+checkScreenWidthAndCloseSidebar();
+window.addEventListener('resize', 
+checkScreenWidthAndCloseSidebar);
+
+function checkScreenWidthAndCloseSidebar()   {
+  if (window.innerWidth <= 660) {
+    closeNav();
+  }
+}
+
+function toggleNav() {
+  if (!sidebarOpen) {
+    openNav();
+  } else {
+    closeNav();
+  }
+}
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("hamburger").style.marginLeft = "200px";
+  sidebarOpen = true;
+}
+
+function closeNav() {
+  if (window.innerWidth > 660) {
+    return; 
+  }
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("hamburger").style.marginLeft = "0"; 
+  sidebarOpen = false;
+  document.removeEventListener('click', closeNavOnOutsideClick);
+}
+
+
+function closeNavOnOutsideClick(event) {
+  if (!event.target.matches('#mySidebar') && !event.target.matches('button')) {
+    closeNav();
+  }
+}
---
+++ assets/login_register.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 19, 2024 at 01:45 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `login_register`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) DEFAULT NULL,
+  `full_name` varchar(128) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `full_name`, `email`, `password`) VALUES
+(NULL, 'Ralph balubayan', 'ralphrolandb30@gmail.com', '$2y$10$MeD67pmj87vXQCe7ZRT3PuRy9lhrgl0rTajmc6naUI9qHOma1xGHe'),
+(NULL, 'ashley ronquillo', 'ronquillo.ashleym@gmail.com', '$2y$10$qECQoPcDZfF31z8c0FmvRu/2ZtkjmFm8.hHEqgvBfx71/8.kUGls6');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
